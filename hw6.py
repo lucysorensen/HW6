6df51c359d80a50364c2e8d05a6a1ea9dc92d998
lucysorensen <ls179@duke.edu>
2013-03-05 10:02:59 -0500

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -1,15 +1,29 @@
 import tweepy
 import time
+import csv
 
 class TwitterUser(object):
 
-    	def __init__(self, screen_name):
+	def __init__(self, screen_name, filename1, filename2):
 		auth = tweepy.OAuthHandler('HT2lRb24tnyJ2THT2iBjmQ', 'sah4nVYDjXUiQPT0tbGwkeOefL2WPQ83hlLShFwioU')
 		auth.set_access_token('29498268-YF6JtHJCL5MwwiYwhdaHVKXOmwV98fmIcwllVKOWw', 'BaWMo1ed44RjJ5tyLVC17wbHLMw11R2mnRhbAyLTbkg')
 		self.api = tweepy.API(auth)
-		#print api.rate_limit_status()
+
 		self.home_followers = self.api.followers_ids(screen_name = screen_name)
 		self.home_friends = self.api.friends_ids(screen_name = screen_name)
+		self.originaluser = self.api.get_user(screen_name = screen_name)
+		
+		self.headers = ['user_id', 'screen_name', 'number followers']
+		writeFile = open(filename1, "wb")
+		self.csvwriter = csv.writer(writeFile)
+		self.csvwriter.writerow(self.headers)
+		self.csvwriter.writerow([self.originaluser.id, self.originaluser.screen_name, self.originaluser.followers_count])
+		
+		self.headers2 = ['starting user_id', 'time initiated']
+		writeFile2 = open(filename2, "wb")
+		self.csvwriter2 = csv.writer(writeFile2)
+		self.csvwriter2.writerow(self.headers2)
+		self.csvwriter2.writerow([self.originaluser.id, time.clock()])
 		
 	def __str__(self, screen_name):
 		return str(screen_name)
@@ -34,6 +48,7 @@ class TwitterUser(object):
 		for user in self.home_followers:
 			object = self.api.get_user(user_id = user)
 			user_followers = object.followers_count
+			self.csvwriter.writerow([object.id, object.screen_name, object.followers_count])
 			if user_followers > followers_twodegrees:
 				most_twodegrees = object
 				followers_twodegrees = user_followers
@@ -41,6 +56,7 @@ class TwitterUser(object):
 			time.sleep(60)
 			for u in seconddegree:
 				object = self.api.get_user(user_id = u)
+				self.csvwriter.writerow([object.id, object.screen_name, object.followers_count])
 				user_followers = object.followers_count
 				if user_followers > followers_twodegrees:
 					most_twodegrees = object
@@ -49,7 +65,7 @@ class TwitterUser(object):
 		return str(most_twodegrees.screen_name)
 	
 	def get_mostactive(self):
-		#This algorithm defines the most active user as the user with the greatest number of status updates / "tweets"
+		#This algorithm defines the most active user as the user whose last (twentieth) tweet on their timeline was created most recently.
 		outlier = self.get_mostpopular()
 		modified_followers = self.home_followers
 		modified_followers.remove(self.api.get_user(screen_name = outlier).id)
@@ -71,17 +87,18 @@ class TwitterUser(object):
 		return str(most_active.screen_name)
 		
 	def get_mostactive_friend(self):
-    	#This algorithm defines the most active user as the user with the greatest number of status updates / "tweets"
+		#This algorithm defines the most active user as the user whose last (twentieth) tweet on their timeline was created most recently.
 		most_active = self.api.get_user(user_id = self.home_followers[0])
 		active_count = most_active.statuses_count
-		for user in self.home_followers:
+		for user in self.home_friends:
 			object = self.api.get_user(user_id = user)
+			self.csvwriter.writerow([object.id, object.screen_name, object.followers_count])
 			if object.statuses_count > active_count:
 				most_active = object
 				active_count = most_active.statuses_count
 		return str(most_active.screen_name)
 
-Lucy = TwitterUser('lucycs36')
+Lucy = TwitterUser('lucycs36', 'hw6_users.csv', 'hw6_crawls.csv')
 print Lucy.get_mostpopular()
 print Lucy.get_mostpopular_twodegrees()
 print Lucy.get_mostactive()
